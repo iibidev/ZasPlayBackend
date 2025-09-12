62f6f6bdffa38686748f7ece6fa0c7a200be5bdd
iibidev <ibai.amo.ballesteros@gmail.com>
2025-09-12 19:41:59 +0200

    changed cookies to token bearer

--- src/index.js
+++ src/index.js
@@ -34,7 +34,6 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.static('public'));
-app.use(cookieParser());
 app.set("view engine", 'ejs');
 app.set("views", path.join(process.cwd(), "src", "view"));
 
--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,18 +1,23 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-    try {
-        const token = req.cookies.token;
+  try {
+    // Leer header Authorization: Bearer <token>
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-        if (!token) {
-            return res.status(401).json({ message: "No token, authorization denied" });
-        }
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.id;
-        next();
-    } catch (err) {
-        console.log(err)
-        return res.status(401).json({ message: "Token inválido o expirado" });
+    if (!token) {
+      return res.status(401).json({ message: "No token, authorization denied" });
     }
+
+    // Verificar token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Guardar info del usuario en req.user
+    req.user = decoded.id;
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json({ message: "Token inválido o expirado" });
+  }
 };
